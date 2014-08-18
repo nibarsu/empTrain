707934f172060b777390cbc869b332b80b8c77af
git master <git.master@localhost>
2014-08-18 15:04:55 +0800

    新增匯入員工資料

--- emp_main.php
+++ emp_main.php
@@ -5,40 +5,40 @@ $dep_options = genDepOption();
 $smarty->assign("dep_options",$dep_options);
 $job_options = genJobOption();
 $smarty->assign("job_options",$job_options);
-//if ($_POST["submit"]=="匯入"){
-//  $sql = " select * from hr_data order by nid";
-//  $sql = " select * from hr_data where emp_id !='A0015'";
-//  $rs = $conn->execute($sql);
-//  while($r = $rs->fetchrow()){
-//    //emp_master
-//    $emp_id = $r["emp_id"];
-//    $cname = $r["cname"];
-//    $edata["emp_id"] = $r["emp_id"];
-//    $edata["cname"] = $r["cname"];
-//    $edata["hiredate"] = $r["hiredate"];
-//    write_to_table($edata,"HR.emp_master");
-//    //emp id
-//    $sql= "select * from emp_master where emp_id ='$emp_id'";
-//    $ers = $conn->execute($sql);
-//    $er = $ers->fetchrow();
-//    $eid = $er["id"];
-//    //dep
-//    $dep_name = $r["dep"];
-//    $sql = "select * from dep_master where dep_name='$dep_name'";
-//    $drs = $conn->execute($sql);
-//    $dr = $drs->fetchrow();
-//    $did =  $dr["id"];
-//    write_to_table(array("eid" => $eid ,"did" =>$did),"HR.emp_dep");
-//
-//    //job
-//    $job_name = $r["job_name"];
-//    $sql ="select * from job_master where job_name ='$job_name'";
-//    $jrs = $conn->execute($sql);
-//    $jr = $jrs->fetchrow();
-//    $jid = $jr["id"];
-//    write_to_table(array("eid"=>$eid ,"jid" =>$jid), "HR.emp_job");
-//  }
-//}
+if ($_POST["submit"]=="匯入"){
+  $sql = " select * from hr_data order by nid";
+  $sql = " select * from hr_data where emp_id !='A0015'";
+  $rs = $conn->execute($sql);
+  while($r = $rs->fetchrow()){
+    //emp_master
+    $emp_id = $r["emp_id"];
+    $cname = $r["cname"];
+    $edata["emp_id"] = $r["emp_id"];
+    $edata["cname"] = $r["cname"];
+    $edata["hiredate"] = $r["hiredate"];
+    write_to_table($edata,"HR.emp_master");
+    //emp id
+    $sql= "select * from emp_master where emp_id ='$emp_id'";
+    $ers = $conn->execute($sql);
+    $er = $ers->fetchrow();
+    $eid = $er["id"];
+    //dep
+    $dep_name = $r["dep"];
+    $sql = "select * from dep_master where dep_name='$dep_name'";
+    $drs = $conn->execute($sql);
+    $dr = $drs->fetchrow();
+    $did =  $dr["id"];
+    write_to_table(array("eid" => $eid ,"did" =>$did),"HR.emp_dep");
+
+    //job
+    $job_name = $r["job_name"];
+    $sql ="select * from job_master where job_name ='$job_name'";
+    $jrs = $conn->execute($sql);
+    $jr = $jrs->fetchrow();
+    $jid = $jr["id"];
+    write_to_table(array("eid"=>$eid ,"jid" =>$jid), "HR.emp_job");
+  }
+}
 if ($_POST["submit"]=="新增"){
   //emp_master
   $fdata = array();
